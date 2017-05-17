b2724fbf015994fce88d1ee9489bd4b1cd9bc284
Michaël Zasso <targos@protonmail.com>
2017-05-17 10:36:48 +0200

    simplify graph (#3)
    * simplify graph
    
    Do not create vertices for ports. Instead, connect the nodes directly
    and store their edges in a separate structure.
    
    * fixup: remove commented code

---
+++ packages/larissa/src/GraphEdge.js
@@ -0,0 +1,25 @@
+// @flow
+
+import type Input from './Input';
+import type Node from './Node';
+import type Output from './Output';
+
+export default class GraphEdge {
+    from: Node;
+    to: Node;
+    connections: Set<string>;
+
+    constructor(from: Node, to: Node) {
+        this.from = from;
+        this.to = to;
+        this.connections = new Set();
+    }
+
+    addConnection(output: Output, input: Input) {
+        const connectionId = output.id + ':' + input.id;
+        if (this.connections.has(connectionId)) {
+            throw new Error(`There is already a connection between ${output.id} and ${input.id}`);
+        }
+        this.connections.add(connectionId);
+    }
+}
