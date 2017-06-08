f45fae104f505d07aa376196ff9507547376a622
Daniel Kostro <kostro.d@gmail.com>
2017-06-08 16:48:37 +0200

    BlockType: add uiSchema field

--- packages/larissa/src/BlockTypes.js
+++ packages/larissa/src/BlockTypes.js
@@ -37,6 +37,7 @@ export class BlockType {
     inputs: Array<Object>;
     outputs: Array<Object>;
     schema: ?Object;
+    uiSchema: ?Object;
     validator: ?(Object) => boolean;
     executor: (Object) => Promise<mixed>;
 
@@ -48,6 +49,7 @@ export class BlockType {
         this.outputs = [];
         this.schema = null;
         this.validator = null;
+        this.uiSchema = null;
 
         if (typeof definition.executor !== 'function') {
             throw new TypeError('block executor must be a function');
@@ -71,6 +73,10 @@ export class BlockType {
         if (definition.options) {
             this.schema = definition.options;
             this.validator = schemaValidator(definition.options);
+        }
+
+        if (definition.uiOptions) {
+            this.uiSchema = definition.uiOptions;
         }
     }
 }
