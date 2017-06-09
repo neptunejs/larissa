78a7a969951f149dbcf4e5bd75e6f156a0d343db
Michaël Zasso <targos@protonmail.com>
2017-06-09 11:28:23 +0200

    pca: return projection as a table

--- packages/larissa-plugin-nodes-mljs/src/blocks/pca.js
+++ packages/larissa-plugin-nodes-mljs/src/blocks/pca.js
@@ -7,7 +7,7 @@ export default {
         // {name: 'variance', type: 'array'},
         // {name: 'model', type: 'pca-model'},
         // {name: 'loadings', type: 'pca-loadings'},
-        {name: 'projection', type: 'matrix'}
+        {name: 'projection', type: 'table'}
     ],
     options: null,
     executor: pca
@@ -17,5 +17,11 @@ async function pca(ctx) {
     const PCA = await import('ml-pca');
     const dataset = ctx.getInput('dataset');
     const result = new PCA(dataset);
-    ctx.setOutput('projection', result.predict(dataset));
+    const projection = result.predict(dataset);
+    const output = {
+        headers: projection[0].map((v, i) => `PC${i + 1}`),
+        data: projection
+    };
+    debugger;
+    ctx.setOutput('projection', output);
 }
